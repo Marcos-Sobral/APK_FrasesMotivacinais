6c795ad004c3f3cf1b53524b1f60a52076838997
Marcos Sobral <vini15oficial@gmail.com>
2022-04-15 15:10:05 -0300

    adicionando Array

--- app/src/main/java/com/frases/frasesmotivacionais/MainActivity.java
+++ app/src/main/java/com/frases/frasesmotivacionais/MainActivity.java
@@ -3,6 +3,7 @@ package com.frases.frasesmotivacionais;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         gerarFrases = findViewById(R.id.gerarFrases);
 
         getSupportActionBar().hide();
+
+        gerarFrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String[] frasesMotivacionais = {"Os limites só existem se você os deixar existir - By: Dragonball",
+                        "O Naruto pode ser um pouco duro as vezes, talvez você não saiba mas ele também cresceu sem pai e nunca teve nenhum amigo em nossa aldeia, na verdade ele nunca conheceu nenhum de seus pais, mas eu nunca vi ele chorar meu palpite... Ele se cansou de chorar - By Kakashi",
+                        "A vida das pessoas não acaba quando elas morrem, mas sim quando perdem a fé - bY Itachi"};
+
+                frases.setText(frasesMotivacionais[2]);
+
+            }
+
+        });
 
     }
 }
