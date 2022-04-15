0384fbea248194ba172a8fd0224c40315db54ce9
Marcos Sobral <vini15oficial@gmail.com>
2022-04-15 14:52:16 -0300

    Frontend

--- app/src/main/java/com/frases/frasesmotivacionais/MainActivity.java
+++ app/src/main/java/com/frases/frasesmotivacionais/MainActivity.java
@@ -3,12 +3,23 @@ package com.frases.frasesmotivacionais;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView frases;
+    private Button gerarFrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frases = findViewById(R.id.frases);
+        gerarFrases = findViewById(R.id.gerarFrases);
+
+        getSupportActionBar().hide();
+
     }
 }
